0523112668ce3362ca3942eba95b12dd4dea423c
Romão moura <c.romaomoura@gmail.com>
2020-07-04 12:17:59 -0300

    Rest testado

---
+++ src/main/java/com/romaomoura/cursospringmvc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.romaomoura.cursospringmvc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET )
+	public String listar() {
+		return "Rest está funcionando";
+	}
+}
