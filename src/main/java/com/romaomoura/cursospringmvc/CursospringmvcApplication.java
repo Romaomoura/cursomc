3bd370df4f0128ca2b27666d0738d487a0d076d9
Romão moura <c.romaomoura@gmail.com>
2020-07-04 11:59:45 -0300

    projeto criado

---
+++ src/main/java/com/romaomoura/cursospringmvc/CursospringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.romaomoura.cursospringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romaomoura/cursospringmvc/CursospringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaomoura.cursospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
