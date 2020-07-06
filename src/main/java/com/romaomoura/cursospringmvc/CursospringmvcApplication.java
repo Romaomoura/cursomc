3df8af0166cea0f77911d5779b2b92a44002a667
Romão moura <c.romaomoura@gmail.com>
2020-07-06 16:07:25 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/romaomoura/cursospringmvc/CursospringmvcApplication.java
+++ src/main/java/com/romaomoura/cursospringmvc/CursospringmvcApplication.java
@@ -8,25 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.romaomoura.cursospringmvc.domain.Categoria;
+import com.romaomoura.cursospringmvc.domain.Produto;
 import com.romaomoura.cursospringmvc.repositories.CategoriaRepository;
+import com.romaomoura.cursospringmvc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursospringmvcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringmvcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(null, "Eletrônicos");
-		Categoria cat2 = new Categoria(null, "Móveis");
-		
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
--- src/main/java/com/romaomoura/cursospringmvc/resources/CategoriaResource.java
+++ src/main/java/com/romaomoura/cursospringmvc/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.romaomoura.cursospringmvc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
