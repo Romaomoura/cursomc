b5a3cd8c75b680baa73a65f3c56a8db27f4c3f31
Romão moura <c.romaomoura@gmail.com>
2020-07-11 09:35:18 -0300

    Implementado metodo insert de categoria

--- src/main/java/com/romaomoura/cursospringmvc/resources/CategoriaResource.java
+++ src/main/java/com/romaomoura/cursospringmvc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.romaomoura.cursospringmvc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.romaomoura.cursospringmvc.domain.Categoria;
 import com.romaomoura.cursospringmvc.services.CategoriaService;
@@ -21,5 +25,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = catServ.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = catServ.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
