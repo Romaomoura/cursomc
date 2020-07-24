97b88dc58c1e3ec33cc86d7a0d890ad23568cc95
Romão moura <c.romaomoura@gmail.com>
2020-07-24 15:02:05 -0300

    Implementando profile de desenvolvimento

---
+++ src/main/java/com/romaomoura/cursospringmvc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.romaomoura.cursospringmvc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.romaomoura.cursospringmvc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbSrvice;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbSrvice.instatieteTestDatabase();
+		return true;
+	}
+
+}
