d6e699966573845f90c4b26437fe091c4a6b1ea0
Romão moura <c.romaomoura@gmail.com>
2020-07-29 14:55:04 -0300

    Implementado Rota de atualização de Token

---
+++ src/main/java/com/romaomoura/cursospringmvc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.romaomoura.cursospringmvc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.romaomoura.cursospringmvc.security.UserSecurity;
+import com.romaomoura.cursospringmvc.security.util.JWTUtil;
+import com.romaomoura.cursospringmvc.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSecurity user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
