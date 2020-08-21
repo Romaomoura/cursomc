2b02c0cc00277fb1f1fe98ccab5a24819cbe63f1
Romão moura <c.romaomoura@gmail.com>
2020-08-21 10:20:48 -0300

    Ajustando configurações de producao.

--- src/main/java/com/romaomoura/cursospringmvc/config/ProdConfig.java
+++ src/main/java/com/romaomoura/cursospringmvc/config/ProdConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.romaomoura.cursospringmvc.services.DBService;
+import com.romaomoura.cursospringmvc.services.EmailService;
+import com.romaomoura.cursospringmvc.services.SmtpEmailService;
 
 @Configuration
 @Profile("prod")
@@ -28,6 +30,11 @@ public class ProdConfig {
 		}
 		dbSrvice.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
